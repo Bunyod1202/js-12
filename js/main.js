11b54fd91c23afdbb3061344e73f3d03ba1384fd
Bunyod <84246289+Bunyood@users.noreply.github.com>
2022-09-29 14:54:33 +0500

    bug fix

--- js/main.js
+++ js/main.js
@@ -2,43 +2,9 @@ let elinput = document.querySelector(".text")
 let ellist = document.querySelector(".list")
 let elform = document.querySelector(".form")
 
-let num = 0 
 
-let array = []
+let array = JSON.parse(window.localStorage.getItem("lastname")) || [];
 
-
-
-
-function adds() {
-   
-  let res = localStorage.getItem("lastname");
-  let rssult = JSON.parse(res)
-  localStorage.setItem("lastname", JSON.stringify(array));
-  
-  console.log(rssult);
-
-  array.forEach(function (item ) {
-    let items = document.createElement("li")
-    let tex = document.createElement("span")
-    let number = document.createElement("span")
-    
-    items.classList.add("list-unstyled", "px-4", "py-2", "alert-success", "mt-3", "border", "border-success", "border-4", "border-start-0","w-50", "border-end-0", "rounded-pill","mx-auto")
-    tex.classList.add("ms-2","text-success")
-    number.classList.add("text-success","fw-bold",)
-    
-    ellist.appendChild(items)
-    items.appendChild(number)
-    items.appendChild(tex)
-    // num++
-    
-     
- 
-  
-
-    tex.textContent = item.name,
-    number.textContent = item.id;
-  })
-}
 elform.addEventListener("submit", function (evt) {
   evt.preventDefault()
   let inputValue = elinput.value;
@@ -47,20 +13,36 @@ elform.addEventListener("submit", function (evt) {
     id: array.length + 1,
     name: inputValue
   }
-  if (inputValue == "") {
-    array.push()
-    
-  } else {
-    
+  if (inputValue !== "") {
     array.push(obj)
   }
 
+  adds(array)
+  window.localStorage.setItem("lastname", JSON.stringify(array));
+})
+
+adds(array)
+
+function adds(arr) {
   ellist.innerHTML = "";
 
-  adds()
-  
-})
-// console.log(array);
+  arr.forEach(function (item ) {
+    let items = document.createElement("li")
+    let tex = document.createElement("span")
+    let number = document.createElement("span")
+    
+    items.classList.add("list-unstyled", "px-4", "py-2", "alert-success", "mt-3", "border", "border-success", "border-4", "border-start-0","w-50", "border-end-0", "rounded-pill","mx-auto")
+    tex.classList.add("ms-2","text-success")
+    number.classList.add("text-success","fw-bold",)
+    
+    
+    number.textContent = item.id;
+    tex.textContent = item.name;
+    
+    items.appendChild(number)
+    items.appendChild(tex)
+    ellist.appendChild(items)
+      
 
-  
-
+  })
+}
